6166dd810f5961d54c6901a59053a8d9a7327f66
Emana Osuwake Edet <143400092+OsuwakeEdet@users.noreply.github.com>
2024-12-07 00:39:31 -0800

    Create 1-variable.js

---
+++ 1-variable.js
@@ -0,0 +1,33 @@
+// VARIABLE IS BASICALLY A BOX FOR HOLDING DATA. you use the "let" or "var" keyword to create variables in JavaScript
+
+// VARIABLE INITIALIZATION
+var firstName = 'Steven';
+let secondName = 'Universe';
+console.log(firstName);
+console.log(secondName);
+
+// VARIABLE DECLARATION
+var thirdName;
+let fourthName;
+console.log();
+console.log();
+
+// VARIABLE ASSIGNMENT
+//  "=" is the assignment operator
+thirdName = 'Greg';
+fourthName = 'Universe';
+console.log();
+console.log();
+// THE VALUES OF VARIABLES CAN BE CHANGED BY ASSIGNNING A NEW VALUE
+thirdName = 'Steven';
+console.log();
+// NOW THE VALUE OF THE VARIABLE "thirdName" HAS CHANGED FROM 'Greg' TO 'Steven'
+
+
+
+// Note: DON'T USE RESERVED WORDS TO CREATE A VARIABLE e.g
+//  let function = 'test';
+//  var default = 100;
+
+// Use camelCase notation for making the variable names the first word will be in all Lowercase 
+// and the First letter of Second word starts with a Uppercase and the rest in Lowercase e.g superMan, starBucks, darkVader, bigMac
