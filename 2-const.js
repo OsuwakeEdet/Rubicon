046a3b7f29228f7b41e642274a2d8a5e6961b626
Emana Osuwake Edet <143400092+OsuwakeEdet@users.noreply.github.com>
2024-12-07 01:08:35 -0800

    Create 2-const.js

---
+++ 2-const.js
@@ -0,0 +1,14 @@
+// "let" keyword is the best way to create variables that their values can be changed 
+// the "const" keyword creates variables whose values can't, e.g
+
+const CHRISTMAS_2024= "25.12.2024";
+console.log(CHRISTMAS_2024);
+
+// use the snake case convention for naming constants, it's like the previous naming convention but this uses uppercase for all the letters in all the words 
+// AND YOU HAVE TO SEPERATE EACH WORD WITH AN UNDERSCORE e.g
+
+const COLOR_GREEN = "green";
+
+// the "const" keyword is used for variables whose values can never be changed or reassigned throughout the program
+// note: IF YOU TRY TO REASSIGN A VARIABLE WITH THE CONST KEYWORD, IT WILL GIVE AN ERROR MESSAGE
+// note: GIVE YOUR VARIABLES DESCRIPTIVE BUT SHORT NAMES TO PROVIDE CONTEXT
