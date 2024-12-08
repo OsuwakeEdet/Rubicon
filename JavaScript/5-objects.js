f1e9f9cca014bef615226968c9c195b57ae9b8cc
Emana Osuwake Edet <143400092+OsuwakeEdet@users.noreply.github.com>
2024-12-08 02:43:18 -0800

    Create 5-objects.js

---
+++ JavaScript/5-objects.js
@@ -0,0 +1,41 @@
+// Objects are refernence tyoes in JavaScript, they are used to represent real objects or noun in real life
+
+// Key: A string (or symbol) that serves as the unique identifier.
+// Value: Any data type (e.g., string, number, array, object, function, etc.) associated with the key.
+
+// EXAMPLE;
+// const user = {
+//   name: "Emana",
+//   age: 21,
+//   isStudent: true
+// };
+
+// Key: "name", "age", "isStudent"
+// Value: "Emana", 21, true
+
+
+let course = {
+name: 'Rubicon',
+owner: 'Emana',
+Goal: 'JavaScript Developer'
+};
+
+console.log(course);
+// the key-value pair
+// e.g (in line 18 of this file, name: 'Rubicon')
+// is called a propery of the object 'course' declared in line 17
+
+// Accessing Key-Value Pair
+// you can access key pairs in an obj
+
+
+
+
+
+
+
+
+
+
+
+
