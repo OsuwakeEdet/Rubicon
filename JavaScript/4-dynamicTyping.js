edfdcf1d10b32565639ea6fef95e3ef9b0a4fcb3
Emana Osuwake Edet <143400092+OsuwakeEdet@users.noreply.github.com>
2024-12-08 02:19:56 -0800

    Create 4-dynamicTyping.js

---
+++ JavaScript/4-dynamicTyping.js
@@ -0,0 +1,9 @@
+// Programming languages that aloow you to initialize a variable to a data type and then reassign it to a different data type are said to be DYNAMICALLY TYPED
+// the type of variable can change at runrime since it is dynamically typed
+let firstName = "Victor";
+console.log(typeof firstname);
+firstName = 18;
+console.log(typeof firstname);
+firstName = true; 
+console.log(typeof firstname);
+// i useed the typeof keyword so we can see the data type at each point in the code
