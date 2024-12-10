6f40614d59833b3f8b67b7c2fbc3847d97600b4f
Toji <143400092+pasafista@users.noreply.github.com>
2024-12-10 13:00:22 -0800

    main

---
+++ JavaScript2/1-arithmeticOperators.js
@@ -0,0 +1,11 @@
+// we operate on our variable, the operators used:
+// arithmetic operator
+// assignment operator
+// comparison operator
+// logical operator
+// bitwise operator
+// they are fundamental building blocks for applications
+
+// for arithmetic operators we have addition +, subtraction -, division / , multiplicatiion *, modular % operators
+ 
+2  + 2;
