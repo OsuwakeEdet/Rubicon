b1b837fc50d64b038257d78944bb90572643c74c
Toji <143400092+pasafista@users.noreply.github.com>
2024-12-10 01:51:45 -0800

    updated 7-function.js and created 8-functionType.js

--- JavaScript/7-functions.js
+++ JavaScript/7-functions.js
@@ -1,1 +1,19 @@
+// Function is the building block of our code, it alows us to group statements perform a task or calculate and return a value
+function sayHello() {
+  console.log("Hello!");
+}
+
+sayHello();
+
+// you can add a parametic variable
+
+function sayHi(name) {
+  // Template literals use backticks (`) to create strings. Inside them, ${} lets you embed variables or expressions
+  // They are more convenient than string concatenation. Without template literals, you'd write the same code like this:
+  // console.log("Hi, " + name);
+
+  console.log("Hi, ${name}"); //
+}
+
+sayHi("Steven");
 
